18df586cdc2f4b04ccd6211ac5e034d345b2bdd7
Luyen KH <kttkh.hcm@poshvn.com>
2026-09-08 18:08:57 +0700

    Fix dau-ra route: pass userName for nav partial

--- routes/dau-ra.js
+++ routes/dau-ra.js
@@ -5,7 +5,7 @@ const { requireLogin } = require("../middleware/auth");
 router.use(requireLogin);
 
 router.get("/dau-ra", (req, res) => {
-  res.render("dau-ra", { user: req.session.user });
+  res.render("dau-ra", { userName: req.session.userName || req.session.user || "" });
 });
 
 module.exports = router;
